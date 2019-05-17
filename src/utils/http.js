d60850b70f8aa277dcff9ce685b66e74f4686393
Adem Ilter <ademilter@gmail.com>
2019-05-17 13:52:43 +0300

    api servisi için header eklendi

--- src/utils/http.js
+++ src/utils/http.js
@@ -5,7 +5,8 @@ class Http {
     if (this.instance) return
 
     this.instance = axios.create({
-      baseURL: process.env.VUE_APP_API_URL
+      baseURL: process.env.VUE_APP_API_URL,
+      headers: { 'x-parola': process.env.VUE_APP_API_PASS }
     })
   }
 }
