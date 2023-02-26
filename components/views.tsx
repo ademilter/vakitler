230c2f1a9524a9599bfdc9cf03a47d528ce3a993
Adem ilter <ademilter@gmail.com>
2023-02-26 22:21:25 +0300

    refactor: view

--- components/views.tsx
+++ components/views.tsx
@@ -2,8 +2,10 @@
 
 import { useEffect } from "react";
 import store from "@/stores/list";
-import ViewTimes from "@/components/view-times";
 import useInterval from "@/lib/use-interval";
+import { cx, themeTimes } from "@/lib/utils";
+import Time from "@/components/time";
+import { TimeNames } from "@/lib/types";
 
 export default function Views() {
   const { fetchData, loading, times, updateTimer } = store();
@@ -14,13 +16,19 @@ export default function Views() {
 
   useInterval(updateTimer, times?.time ? 1000 : null);
 
+  if (!times) return null;
+
+  const styleCnt = themeTimes[times.time.now];
+
   return (
     <div>
       {loading ? (
         <div>Loading...</div>
       ) : (
-        <div className="h-screen">
-          <ViewTimes />
+        <div className={cx("grid h-screen", styleCnt)}>
+          {Object.keys(times.today).map((key) => {
+            return <Time key={key} time={key as TimeNames} />;
+          })}
         </div>
       )}
     </div>
