1ca98fa1fbe79b24a7f51b238add90a42352a6f5
Adem ilter <ademilter@gmail.com>
2023-03-06 10:15:26 +0300

    style: timepicker

--- components/time-picker.tsx
+++ components/time-picker.tsx
@@ -1,25 +1,11 @@
-import React, { useCallback, useContext, useEffect } from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
 import { DateTime } from "luxon";
 import { CommonStoreContext } from "@/stores/common";
-import { cx } from "@/lib/utils";
-import { TimeNames } from "@/lib/types";
-
-const buttonBaseClasses = "rounded py-1 px-2 text-xs font-bold text-white";
-
-const backgroundColors = {
-  [TimeNames.Imsak]: "bg-sky-700",
-  [TimeNames.Gunes]: "bg-orange-700",
-  [TimeNames.Ogle]: "bg-yellow-700",
-  [TimeNames.Ikindi]: "bg-amber-700",
-  [TimeNames.Aksam]: "bg-blue-700",
-  [TimeNames.Yatsi]: "bg-indigo-700",
-};
 
 export default function TimePicker() {
-  const { times, localTime, setLocalTime, fetchData } =
-    useContext(CommonStoreContext);
-  const now = times?.time?.now;
-  const backgroundColor = now ? backgroundColors[now] : "";
+  const { localTime, setLocalTime, fetchData } = useContext(CommonStoreContext);
+
+  const [time, setTime] = useState<string>(localTime.toFormat("HH:mm"));
 
   useEffect(() => {
     const rawSettings = localStorage.getItem("VAKITLER_SETTINGS");
@@ -28,47 +14,26 @@ export default function TimePicker() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [localTime]);
 
-  const onForwardClick = useCallback(
-    () => setLocalTime(localTime.plus({ hours: 1 })),
+  const onSetTime = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const value = event.target.value;
+      setTime(value);
+      const time = DateTime.fromFormat(value, "HH:mm");
+      setLocalTime(time);
+    },
     [localTime, setLocalTime]
-  );
-  const onBackClick = useCallback(
-    () => setLocalTime(localTime.minus({ hours: 1 })),
-    [localTime, setLocalTime]
-  );
-  const onNowClick = useCallback(
-    () => setLocalTime(DateTime.local()),
-    [setLocalTime]
   );
 
+  const onNowClick = useCallback(() => {
+    setTime(DateTime.local().toFormat("HH:mm"));
+    setLocalTime(DateTime.local());
+  }, [setLocalTime]);
+
   return (
-    <div className="absolute top-2 right-2 z-30 rounded p-2 shadow-lg">
-      <div className="flex items-center">
-        <button
-          onClick={onBackClick}
-          className={cx(buttonBaseClasses, backgroundColor)}
-        >
-          Geri
-        </button>
-        <p className="mx-2 text-xs">{localTime.toFormat("dd MMMM, HH:mm")}</p>
-        <button
-          onClick={onForwardClick}
-          className={cx(buttonBaseClasses, backgroundColor)}
-        >
-          İleri
-        </button>
-      </div>
-      <div className="w-full">
-        <button
-          onClick={onNowClick}
-          className={cx(
-            buttonBaseClasses,
-            backgroundColor,
-            "mt-2 flex w-full justify-center"
-          )}
-        >
-          Şimdi
-        </button>
+    <div className="fixed top-0 right-0 z-30 rounded p-4 shadow-lg">
+      <div className="flex items-center gap-2">
+        <input type="time" value={time} onChange={onSetTime} />
+        <button onClick={onNowClick}>Clear</button>
       </div>
     </div>
   );
