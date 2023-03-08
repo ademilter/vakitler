b93eafda0ac8e0a1767817ddd739f0a67afabe61
Adem ilter <ademilter@gmail.com>
2023-03-09 01:12:45 +0300

    style: hide time-travel

--- components/time-travel.tsx
+++ components/time-travel.tsx
@@ -1,10 +1,13 @@
-import React, { useCallback, useContext } from "react";
+import React, { useCallback, useContext, useState } from "react";
 import { CommonStoreContext } from "@/stores/common";
+import { cx } from "@/lib/utils";
 
 const shouldShowTimePicker = process.env.NODE_ENV !== "production";
 
 export default function TimeTravel() {
   const { devLocalTime, setDevLocalTime } = useContext(CommonStoreContext);
+
+  const [show, setShow] = useState(false);
 
   const onNowClick = useCallback(() => {
     setDevLocalTime([0, 0, 0]);
@@ -13,8 +16,22 @@ export default function TimeTravel() {
   if (!shouldShowTimePicker) return null;
 
   return (
-    <div className="fixed bottom-0 right-0 z-30">
-      <div className="flex items-center gap-2 rounded-lg bg-white px-3 py-2">
+    <div
+      className={cx(
+        "transform-x fixed bottom-2 right-2 z-30",
+        show ? "translate-x-0" : "translate-x-full"
+      )}
+    >
+      <div
+        className={cx("flex items-center gap-2 rounded-lg bg-white px-3 py-2")}
+      >
+        <button
+          className="-ml-3 flex h-8 w-3 rounded-lg"
+          onClick={() => {
+            setShow(!show);
+          }}
+        />
+
         <input
           type="number"
           className="h-8 w-12 rounded-lg bg-zinc-100 pl-2"
