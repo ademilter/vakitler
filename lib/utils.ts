98af00ed382474e53db7508f44597f72d31419f4
Abdulbaki Dursun <eggsydev@gmail.com>
2023-03-11 23:16:00 +0300

    chore: remove unused variable

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,10 +9,6 @@ export function cx(...inputs: ClassValue[]): string {
 }
 
 export function secondSplit(second: number): TypeTimer {
-  let pad = (x: number): string => {
-    return x < 10 ? `0${x}` : `${x}`;
-  };
-
   return [
     Math.floor(second / 3600),
     Math.floor((second % 3600) / 60),
