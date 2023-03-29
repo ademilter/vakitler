28edefbd23986c59f7c0cfaf1f0ea018fa5fec3d
Adem ilter <ademilter@gmail.com>
2023-03-30 01:41:27 +0300

    refactor: meta

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
-const nextTranslate = require("next-translate-plugin")
+const nextTranslate = require("next-translate-plugin");
 
 const withPWA = require("next-pwa")({
   dest: "public",
+  disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
 });
 
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = withPWA({
   reactStrictMode: false,
 });
- 
-module.exports = nextTranslate(nextConfig)
+
+module.exports = nextTranslate(nextConfig);
