b7d1309256638a27d9ffcad7716b764cf45870e8
code-a-man <43219246+code-a-man@users.noreply.github.com>
2023-03-29 22:07:57 +0300

    add service worker

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
-const nextTranslate = require("next-translate-plugin");
+const nextTranslate = require("next-translate-plugin")
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
   reactStrictMode: false,
-};
-
-module.exports = nextTranslate(nextConfig);
+});
+ 
+module.exports = nextTranslate(nextConfig)
