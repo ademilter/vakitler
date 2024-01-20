ab9fa6d8d2ad0684af099caf135810d8fca1a4d3
Adem ilter <ademilter@gmail.com>
2024-01-20 22:53:05 +0300

    style: letter space

--- components/time-summary-timer.tsx
+++ components/time-summary-timer.tsx
@@ -10,55 +10,70 @@ export default function TimeSummaryTimer() {
     <div className="flex items-baseline text-5xl font-light gap-2">
       {timer[0] === 0 && timer[1] === 0 ? (
         // 30 second
-        <Trans
-          ns="common"
-          i18nKey="timerSecond"
-          components={[<ValueComp key="second" className="tabular-nums" />]}
-          values={{ second: timer[2] }}
-        />
+        <KeyValueComp>
+          <Trans
+            ns="common"
+            i18nKey="timerSecond"
+            components={[<ValueComp key="second" className="tabular-nums" />]}
+            values={{ second: timer[2] }}
+          />
+        </KeyValueComp>
       ) : timer[0] === 0 ? (
         // 30 minute
-        <Trans
-          ns="common"
-          i18nKey="timerMinute"
-          components={[<ValueComp key="minute" />]}
-          values={{ minute: timer[1] }}
-        />
+        <KeyValueComp>
+          <Trans
+            ns="common"
+            i18nKey="timerMinute"
+            components={[<ValueComp key="minute" />]}
+            values={{ minute: timer[1] }}
+          />
+        </KeyValueComp>
       ) : timer[1] === 0 ? (
         // 2 hour 0 minute
-        <Trans
-          ns="common"
-          i18nKey="timerHour"
-          components={[<ValueComp key="hour" />]}
-          values={{ hour: timer[0] }}
-        />
+        <KeyValueComp>
+          <Trans
+            ns="common"
+            i18nKey="timerHour"
+            components={[<ValueComp key="hour" />]}
+            values={{ hour: timer[0] }}
+          />
+        </KeyValueComp>
       ) : (
         // 2 hour 30 minute
         <>
-          <span className="flex items-baseline gap-0.5">
+          <KeyValueComp>
             <Trans
               ns="common"
               i18nKey="timerHour"
               components={[<ValueComp key="hour" />]}
               values={{ hour: timer[0] }}
             />
-          </span>
-          <span className="flex items-baseline gap-0.5">
+          </KeyValueComp>
+          <KeyValueComp>
             <Trans
               ns="common"
               i18nKey="timerMinute"
               components={[<ValueComp key="minute" />]}
               values={{ minute: timer[1] }}
             />
-          </span>
+          </KeyValueComp>
         </>
       )}
     </div>
   );
 }
 
+function KeyValueComp({
+  className,
+  ...props
+}: HTMLAttributes<HTMLSpanElement>) {
+  return (
+    <span className={cx("flex items-baseline gap-0.5", className)} {...props} />
+  );
+}
+
 function ValueComp({ className, ...props }: HTMLAttributes<HTMLSpanElement>) {
   return (
-    <b className={cx("font-semibold text-[1.2em]", className)} {...props} />
+    <b className={cx("font-semibold text-[1.1em]", className)} {...props} />
   );
 }
