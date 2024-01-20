3a214bd4afe0f1e4492b7b2ac91d4f2878ef307a
Adem ilter <ademilter@gmail.com>
2024-01-21 00:00:16 +0300

    fix: theme color

--- components/layout/main.tsx
+++ components/layout/main.tsx
@@ -7,26 +7,26 @@ import { useTheme } from "next-themes";
 
 const theme = {
   [TimeNames.Imsak]:
-    "bg-sky-300/30 text-sky-800 dark:bg-sky-600/20 dark:text-sky-200",
+    "bg-sky-300/30 text-sky-800 dark:bg-sky-500/20 dark:text-sky-200",
   [TimeNames.Gunes]:
-    "bg-orange-300/30 text-orange-800 dark:bg-orange-600/20 dark:text-orange-200",
+    "bg-orange-300/30 text-orange-800 dark:bg-orange-500/20 dark:text-orange-200",
   [TimeNames.Ogle]:
-    "bg-yellow-300/30 text-yellow-800 dark:bg-yellow-600/20 dark:text-yellow-200",
+    "bg-yellow-300/30 text-yellow-800 dark:bg-yellow-500/20 dark:text-yellow-200",
   [TimeNames.Ikindi]:
-    "bg-rose-300/30 text-rose-800 dark:bg-rose-600/20 dark:text-rose-200",
+    "bg-rose-300/30 text-rose-800 dark:bg-rose-500/20 dark:text-rose-200",
   [TimeNames.Aksam]:
-    "bg-blue-300/30 text-blue-800 dark:bg-blue-600/20 dark:text-blue-200",
+    "bg-blue-300/30 text-blue-800 dark:bg-blue-500/20 dark:text-blue-200",
   [TimeNames.Yatsi]:
-    "bg-indigo-300/30 text-indigo-800 dark:bg-indigo-600/20 dark:text-indigo-200",
+    "bg-indigo-300/30 text-indigo-800 dark:bg-indigo-500/20 dark:text-indigo-200",
 };
 
 const color = {
-  [TimeNames.Imsak]: ["#daf2fe", "#1a2d3f"],
-  [TimeNames.Gunes]: ["#feead6", "#42261d"],
-  [TimeNames.Ogle]: ["#fef6cc", "#3c2f1e"],
-  [TimeNames.Ikindi]: ["#ffe4e7", "#411c25"],
-  [TimeNames.Aksam]: ["#e0edff", "#1c2745"],
-  [TimeNames.Yatsi]: ["#e4e8fe", "#242144"],
+  [TimeNames.Imsak]: ["#daf2fe", "#311808"],
+  [TimeNames.Gunes]: ["#feead6", "#311808"],
+  [TimeNames.Ogle]: ["#fef6cc", "#2f2407"],
+  [TimeNames.Ikindi]: ["#ffe4e7", "#310e14"],
+  [TimeNames.Aksam]: ["#e0edff", "#0d1a32"],
+  [TimeNames.Yatsi]: ["#e4e8fe", "#141431"],
 };
 
 export default function MainPage({ children }: { children: ReactNode }) {
@@ -42,7 +42,7 @@ export default function MainPage({ children }: { children: ReactNode }) {
     : "";
 
   return (
-    <div className={cx("fixed bg-white inset-0")}>
+    <div className={cx("fixed bg-white dark:bg-black inset-0")}>
       <Head>
         {themeColor && <meta name="theme-color" content={themeColor} />}
       </Head>
