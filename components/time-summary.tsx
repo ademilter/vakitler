c18355d0e1bbf1073211ef9a231ca9ca2e3fc6ff
Adem ilter <ademilter@gmail.com>
2024-01-21 22:03:59 +0300

    fix summary space

--- components/time-summary.tsx
+++ components/time-summary.tsx
@@ -37,7 +37,7 @@ export default function TimeSummary() {
 
   return (
     <motion.div {...containerAnim}>
-      <Container className="flex h-full gap-1 flex-col items-center justify-center pt-10">
+      <Container className="flex h-full gap-1 flex-col items-center justify-center pt-24 pb-14">
         <span className="flex capitalize text-xl font-semibold">
           <Trans
             ns={"common"}
