18e08507393a600a48203474251c9702e474a2b7
Adem ilter <ademilter@gmail.com>
2024-03-20 18:30:33 +0300

    fix: timer

--- components/time-summary-timer.tsx
+++ components/time-summary-timer.tsx
@@ -8,45 +8,53 @@ export default function TimeSummaryTimer() {
 
   return (
     <div className="flex items-baseline text-5xl md:text-6xl font-light gap-2">
-      {timer[0] > 0 && (
-        <KeyValueComp>
-          <Trans
-            ns="common"
-            i18nKey="timerHour"
-            components={[<ValueComp key="hour" />]}
-            values={{ hour: timer[0] }}
-          />
-        </KeyValueComp>
-      )}
       {timer[0] > 0 ? (
-        <KeyValueComp>
-          <Trans
-            ns="common"
-            i18nKey="timerMinute"
-            components={[<ValueComp key="minute" />]}
-            values={{ minute: timer[1] }}
-          />
-        </KeyValueComp>
-      ) : (
-        timer[1] > 0 && (
+        <>
           <KeyValueComp>
             <Trans
               ns="common"
-              i18nKey="timerMinute"
-              components={[<ValueComp key="minute" />]}
-              values={{ minute: timer[1] }}
+              i18nKey="timerHour"
+              components={[<ValueComp key="hour" />]}
+              values={{ hour: timer[0] }}
             />
           </KeyValueComp>
-        )
+          {timer[1] > 0 && (
+            <KeyValueComp>
+              <Trans
+                ns="common"
+                i18nKey="timerMinute"
+                components={[<ValueComp key="minute" />]}
+                values={{ minute: timer[1] }}
+              />
+            </KeyValueComp>
+          )}
+        </>
+      ) : (
+        <>
+          {timer[1] > 0 && (
+            <KeyValueComp>
+              <Trans
+                ns="common"
+                i18nKey="timerMinute"
+                components={[<ValueComp key="minute" />]}
+                values={{ minute: timer[1] }}
+              />
+            </KeyValueComp>
+          )}
+          {timer[1] === 0 && (
+            <KeyValueComp>
+              <Trans
+                ns="common"
+                i18nKey="timerSecond"
+                components={[
+                  <ValueComp key="second" className="tabular-nums" />,
+                ]}
+                values={{ second: timer[2] }}
+              />
+            </KeyValueComp>
+          )}
+        </>
       )}
-      {/*<KeyValueComp>
-        <Trans
-          ns="common"
-          i18nKey="timerSecond"
-          components={[<ValueComp key="second" className="tabular-nums" />]}
-          values={{ second: timer[2] }}
-        />
-      </KeyValueComp>*/}
     </div>
   );
 }
